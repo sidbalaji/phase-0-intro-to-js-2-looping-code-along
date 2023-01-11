1d7a05c31c0a62f6b8d64c5a45dc49d0fdebd0d7
Siddarth Balaji <sid.balaji@berkeley.edu>
2023-01-11 16:51:54 -0500

    completed lab with all tests passing

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    let msgList = []
+    for(let i = 0; i<names.length; i++){
+        msgList.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        
+    }
+    return msgList
+}
+
+function countDown(num){
+    while(num>=0){
+        console.log(num)
+        num -= 1
+    }
+}
